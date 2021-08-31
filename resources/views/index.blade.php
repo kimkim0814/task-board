53b3d13dab6147b2c31e8ff7b3dab54cab134a42
kimkim0814 <tnryj505@gmail.com>
2021-08-31 23:31:03 +0900

    ルーティング設定 TOPページ作成
    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="md:mx-4 relative overflow-hidden">
+    <main class="h-full flex flex-col overflow-auto">
+        
+    </main>
+</div>
+@endsection
