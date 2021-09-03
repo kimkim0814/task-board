7ff53515a17041e603b7b0ed89fcc82781e7b383
kimkim0814 <tnryj505@gmail.com>
2021-09-03 23:38:00 +0900

    ボード修正　タスクの保存実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,4 +14,17 @@ class TaskController extends Controller
         return view('tasks.index', compact('tasks'));
 
     }
+
+     public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => ['required', 'string', 'max:56'],
+            'description' => ['required', 'string'],
+            'status_id' => ['required', 'exists:statuses,id']
+        ]);
+
+        return $request->user()
+            ->tasks()
+            ->create($request->only('title', 'description', 'status_id'));
+    }
 }
--- resources/js/components/TaskBoard.vue
+++ resources/js/components/TaskBoard.vue
@@ -6,13 +6,9 @@
       class="mr-6 w-4/5 max-w-xs flex-1 flex-shrink-0"
     >
       <div class="rounded-md shadow-md overflow-hidden">
-        <div class="p-3 flex justify-between items-baseline bg-blue-800 ">
-          <h4 class="font-medium text-white">
-            {{ status.title }}
-          </h4>
-          <button class="py-1 px-2 text-sm text-orange-500 hover:underline">
-            タスク追加
-          </button>
+        <div class="p-3 flex justify-between items-baseline bg-blue-800">
+          <h4 class="font-medium text-white">{{ status.title }}</h4>
+          <button class="py-1 px-2 text-sm text-orange-500 hover:underline">タスク追加</button>
         </div>
         <div class="p-2 flex-1 flex flex-col h-full overflow-x-hidden overflow-y-auto bg-blue-100">
           <div
@@ -20,44 +16,28 @@
             :key="task.id"
             class="mb-3 p-3 h-24 flex flex-col bg-white rounded-md shadow transform hover:shadow-md cursor-pointer"
           >
-            <span class="block mb-2 text-xl text-gray-900">
-              {{ task.title }}
-            </span>
-            <p class="text-gray-700 truncate">
-              {{ task.description }}
-            </p>
+            <span class="block mb-2 text-xl text-gray-900">{{ task.title }}</span>
+            <p class="text-gray-700 truncate">{{ task.description }}</p>
           </div>
 
+          <!-- あとで、ここにタスク追加フォームを入れます-->
 
-<AddTaskForm
-    v-if="newTaskForStatus === status.id"
-    :status-id="status.id"
-    v-on:task-added="handleTaskAdded"
-    v-on:task-canceled="closeAddTaskForm"
-  />
-  <draggable
-                                    class="flex-1 overflow-hidden"
-                                    v-model="status.tasks"
-                                    v-bind="taskDragOptions"
-                                    @end="handleTaskMoved"
-                                >
-                                    <transition-group
-                                        class="flex-1 flex flex-col h-full overflow-x-hidden overflow-y-auto rounded shadow-xs"
-                                        tag="div"
-                                    >
+          <AddTaskForm
+            v-if="newTaskForStatus === status.id"
+            :status-id="status.id"
+            v-on:task-added="handleTaskAdded"
+            v-on:task-canceled="closeAddTaskForm"
+          />
           <div
-            v-show="!status.tasks.length"
+            v-show="!status.tasks.length && newTaskForStatus !== status.id"
             class="flex-1 p-4 flex flex-col items-center justify-center"
           >
             <span class="text-gray-600">No tasks yet</span>
             <button
               class="mt-1 text-sm text-orange-600 hover:underline"
-            >
-              追加
-            </button>
+              @click="openAddTaskForm(status.id)"
+            >Add one</button>
           </div>
-</transition-group>
-  </draggable>
         </div>
       </div>
     </div>
@@ -65,12 +45,10 @@
 </template>
 
 <script>
-import AddTaskForm from "./AddTaskForm"; //コンポーネントをインポートする
-import draggable from "vuedraggable";
+import AddTaskForm from "./AddTaskForm"
 
 export default {
-  components: { AddTaskForm ,
-                draggable}, // 登録
+  components: { AddTaskForm }, // 登録
 
   props: {
     initialData: Array
@@ -86,6 +64,7 @@ export default {
     // ステータスを「クローン」して、変更時にプロップを変更しないように
     this.statuses = JSON.parse(JSON.stringify(this.initialData));
   },
+
   methods: {
     // statusIdを設定し、フォームを表示
     openAddTaskForm(statusId) {
@@ -107,7 +86,7 @@ export default {
 
       // AddTaskFormを閉じる
       this.closeAddTaskForm();
-    },
+    }
   }
 };
 </script>
